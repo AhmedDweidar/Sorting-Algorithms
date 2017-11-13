3358ad69af50c59cdfd7e1352f7171b006f1b1cf
AhmedDweidar <32782785+AhmedDweidar@users.noreply.github.com>
2017-11-13 17:58:08 -0500

    added files

---
+++ Bubble Sort/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // using the generic class with a double array
+            Sorter<double> sort1 = new Sorter<double>();
+            double[] doubleArray = { 16.8, 1.2, 17.3, 12.5, 45.7, 88.9, 13.9, 1.2, 10.2, 47.3, 101.9, 1.2 };
+            sort1.BubbleSorter(doubleArray);
+            sort1.Print(doubleArray);
+
+            Console.WriteLine();
+
+            // using the generic class with an int array
+            Sorter<int> sort2 = new Sorter<int>();
+            int[] intArray = { 45, 7, 23, 9, 8, 16, 74, 105, 89, -2, 1, 33 };
+            sort2.BubbleSorter(intArray);
+            sort2.Print(intArray);
+
+            Console.WriteLine();
+            Console.Write("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+} 
+            
+            
---
+++ Bubble Sort/Sorter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    class Sorter<T> where T : IComparable<T> 
+    {
+        public void BubbleSorter(T[] array)  
+        {
+            // variable used during swap
+            T temp; 
+
+            // for loop to control number of passes through array
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                // for loop to control comparison between adjacent elements
+                for (int j = 0; j < array.Length - 1 - i; j++)
+                {
+                    // compare adjacent numbers, swap if the first one is greater than the second one
+                    if ((array[j].CompareTo(array[j + 1]) > 0)) 
+                    {
+                        temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        // method to print the sorted array
+        public void Print(T[] array)
+        {
+            Console.WriteLine("The sorted array is: ");
+            foreach (T number in array)
+            {
+                Console.Write(number + ", ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
