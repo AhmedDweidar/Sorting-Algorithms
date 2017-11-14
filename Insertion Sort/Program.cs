4e80faab677aa6826a48e81af04857e525d621e4
AhmedDweidar <32782785+AhmedDweidar@users.noreply.github.com>
2017-11-13 22:53:41 -0500

    added source files to insertion sort folder

---
+++ Insertion Sort/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sorter<int> sort = new Sorter<int>();
+            int[] numbers = { 105, 6, 24, 78, 0, -5, 12, 3 };
+
+            sort.InsertionSorter(numbers);
+            sort.Print(numbers);
+
+            Console.WriteLine();
+            Console.Write("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Insertion Sort/Sorter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertionSort
+{
+    class Sorter<T> where T : IComparable<T> 
+    {
+        public void InsertionSorter(T[] array)
+        {
+            T temp;
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                int j = i;
+
+                while (j > 0 && (array[j].CompareTo(array[j - 1]) < 0))
+                {
+                    temp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = temp;
+                    j--;
+                }
+            }
+        }
+
+        public void Print(T[] array)
+        {
+            Console.WriteLine("The sorted array is: ");
+            foreach (T number in array)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
