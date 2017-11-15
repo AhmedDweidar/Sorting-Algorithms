b23457cf833a8f25a18a8ec71b220b87cf52cc85
AhmedDweidar <32782785+AhmedDweidar@users.noreply.github.com>
2017-11-14 20:52:28 -0500

    testing .gitignore file

--- Test/Sorter.cs
+++ Test/Sorter.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+// hi how are u
 namespace MaxMin
 {
     class Sorter<T> where T : IComparable<T>
