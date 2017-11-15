8cc3887312cedafd73ee1fcd8655b6673cf23a85
Ahmed Dweidar <32782785+AhmedDweidar@users.noreply.github.com>
2017-11-14 19:27:34 -0500

    Add files via upload

---
+++ Test/Sorter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxMin
+{
+    class Sorter<T> where T : IComparable<T>
+    {
+        public T FindMax(T[] array)
+        {
+            T max = array[0];
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if ((array[i].CompareTo(max)) > 0)
+                {
+                    max = array[i];
+                }
+            }
+
+            return max;
+        }
+
+        public T FindMin(T[] array)
+        {
+            T min = array[0];
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if ((array[i].CompareTo(min)) < 0)
+                {
+                    min = array[i];
+                }
+            }
+
+            return min;
+        }
+    }
+}
