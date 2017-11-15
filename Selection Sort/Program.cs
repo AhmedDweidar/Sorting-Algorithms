f490e0039ddd55da5e0defa46f87623e6ecec4b4
AhmedDweidar <32782785+AhmedDweidar@users.noreply.github.com>
2017-11-15 15:19:50 -0500

    added selection sort files

---
+++ Selection Sort/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sorter<double> sort = new Sorter<double>();
+            double[] doubleArray = { 16.8, 1.2, 17.3, 12.5, -7.2, 45.7, 1.3, 88.9, 1.2, 10.2, -5.8, 47.3, 101.9, 1.2 };
+            sort.SelectionSort(doubleArray);
+            sort.Print(doubleArray);
+
+            Console.WriteLine();
+            Console.Write("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Selection Sort/Sorter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSort
+{
+    class Sorter<T> where T : IComparable<T> 
+    {
+        public void SelectionSort(T[] array)
+        {
+            T temp; // temp variable used during swap
+            int minPosition; // index of the minimum element
+
+            // controls passes through array
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                minPosition = i; // set the min element at the current i position
+
+                // loop through the array and check for smaller element
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    // if element at j is smaller than the current min, update min
+                    if ((array[j].CompareTo(array[minPosition]) < 0))
+                    {
+                        minPosition = j;
+                    }
+                }
+
+                // swap the element at min position with i
+                temp = array[i];
+                array[i] = array[minPosition];
+                array[minPosition] = temp;
+            }
+        }
+
+        // method to print the sorted array
+        public void Print(T[] array)
+        {
+            Console.WriteLine("The sorted array is: ");
+            foreach (T number in array)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
